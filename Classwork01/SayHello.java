45dfd6a4c7084ded11a646866e9df81bf232b127
Matt McPartlan <mmcpartlan98@gmail.com>
2018-09-05 12:47:49 -0700

    Removed Junk
    Corrected file structure

--- Classwork01/SayHello.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class SayHello {
-  public static void main (String[] args) {
-    Scanner myInput = new Scanner(System.in);
-    String inputName = myInput.nextLine();
-    System.out.println("Hello, " + inputName);
-  }
-}
